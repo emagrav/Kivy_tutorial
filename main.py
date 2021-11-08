2297768982f7ed214ee7c009cc9d5da442209c1a
emagrav <emagrav@gmail.com>
2021-11-08 22:55:25 +0100

    gestito transizioni tra schermate

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
-
+from kivy.uix.screenmanager import Screen, ScreenManager, WipeTransition
 
 # il popup non è un sotto oggetto di MainWindow ma un altro oggetto
 # per questo definiamo una classe e, parallelamente, nel file kv
@@ -13,8 +13,12 @@ class CustomPopup(Popup):
 
     def dismiss_popup(self):
         self.dismiss()
-
-class MainWindow(BoxLayout):
+# Ora per creare più schermate in un'applicazione, anziché ereditare
+# da un layout (in questo caso BoxLayout), facciamo ereditare da Screen
+# e per gestire le transizioni da uno screen all'altro dobbiamo definire
+# una classe che eredita da ScreenManager
+#class MainWindow(BoxLayout):
+class MainWindow(Screen):
     # qui specifico l'id dell'oggetto che si trova subito sotto la 
     # definizione di <MainWindow> nel file kv che voglio accedere
     submit_button = ObjectProperty(None)
@@ -44,16 +48,54 @@ class MainWindow(BoxLayout):
         print("Riga:", riga)
         with open("out/form.txt",'w') as f:
             f.write(riga)
+        
+        #lanciamo la seconda schermata
+        sm.switch_to(screens[1], direction="left")
+        #sm.current = "secondwindow" 
 
+class SecondWindow(Screen):
+    def go_back(self):
+        #sm.current = "mainwindow"
+        sm.switch_to(screens[0], direction="right")
+
+class WindowManager(ScreenManager):
+    pass
+
+# qualora invece si volesse chiamare il file .kv ad es. "style.kv",
+# va eseguita questa istruzione
+# NOTA: anche se non ha molto senso, dal momento che si gestiscono più
+# schermate, va necessariamente utilizzata questa istruzione. 
+# L'importante è che il file kv non si chiami allo stesso modo di prima, main.kv.
+# altrimenti funziona a metà e il popup non viene più visualizzato!
+# Quindi chiamarlo style.kv
+kv = Builder.load_file("style.kv")
+
+# instanziamo il nostro ScreenManager
+# se non ci piace la transizione di default possiamo utilizzarne altre
+# come ad esempio la WipeTransition
+sm = WindowManager()
+#sm = WindowManager(transition=WipeTransition())
+
+# definiamo una lista di screen con in ordine MainWindow a cui associamo il nome
+# mainwindow e SecondWindow con nome secondwindow per consentire allo screen 
+# manager per dentificare le schermate
+screens = [MainWindow(name='mainwindow'), SecondWindow(name='secondwindow')]
+
+# aggiungiamo le singole schermate allo screen manager
+for screen in screens:
+    sm.add_widget(screen)
+
+# decidiamo quale schermata deve partire per prima
+sm.current = 'mainwindow'
+
+# definire una classe che eredita da App in cui il metodo build
+# restituisca la prima o l'unica schermata dell'applicazione
 # il nome della classe deve iniziare con Main come il file .kv
 # può anche chiamarsi, quindi, semplicemente Main
 class MainApp(App):
     def build(self):
-        return MainWindow()
-
-# qualora invece si volesse chiamare il file .kv ad es. "style.kv",
-# va eseguita questa istruzione
-#kv = Builder.load_file("style.kv")
+        #return MainWindow()
+        return sm
 
 sample_app = MainApp()
 sample_app.run()
